db405a7bb02162a6a724f62eb4285a0f8b10398e
yolios <julius.haeuslein@web.de>
2018-06-09 14:33:22 +0200

    Add serial communication. FAULT: only reading one line!

--- GsmWrap.cpp
+++ GsmWrap.cpp
@@ -1,21 +1,67 @@
-GsmWrap::GsmWrap(){
-	rx = STD_RX_PIN;
-	tx = STD_TX_PIN;
-	baudrate = STD_BAUD;
+#include "GsmWrap.h"
+
+GsmWrap::GsmWrap(SoftwareSerial* softser){
+	ss = softser;
+}
+
+//-------------------PUBliC---------------------
+
+void GsmWrap::begin(int baud){
+	ss->begin(baud);
+}
+
+bool GsmWrap::serialAvailable(){
+	if(ss->available() > 0){
+		return true;
+	}
+	else{
+		return false;
+	}
+}
+
+void GsmWrap::serialWrite(const char* message){
+	Serial.print("serialWrite: ");
+	Serial.println(message);
+	ss->print(message);
+}
+
+int GsmWrap::serialRead(){
+	clearInBuffer();
 	
-	beginSerial();
-}
-
-GsmWrap::GsmWrap(int rxPin, int txPin, int baud){
-	rx = rxPin;
-	tx = txPin;
-	baudrate = baud;
+	char input;
+	char* buffPtr = inBuffer;
 	
-	beginSerial();
+	unsigned long int timeout = millis() + SERIAL_READ_TIMEOUT;
+	while((millis() < timeout) && (buffPtr < (inBuffer + sizeof(inBuffer) - 1))){
+		input = ss->read();
+		//Serial.println((int)input);
+		if(input != -1){
+			*buffPtr = (char)input;
+			buffPtr++;
+			timeout = millis() + SERIAL_READ_TIMEOUT;
+		}
+	}
+	//*buffPtr = '\0'; not needed due to memset with '\0'
+	return buffPtr - inBuffer;
 }
 
-GsmWrap::beginSerial(){
-	ss = new SoftwareSerial(rx, tx);
-	ss.begin(baud);
+char* GsmWrap::getInBuffer(){
+	return inBuffer;
 }
 
+void GsmWrap::printBuffer(){
+	for(int i=0; i<BUFFSIZE_IN; i++){
+		//Serial.print(i);
+		//Serial.print(": ");
+		Serial.write(inBuffer[i]);
+	}
+}
+
+//----------------PRIVATE-------------------
+
+void GsmWrap::clearInBuffer(){
+	//std::memset(inBuffer, '\0', sizeof(inBuffer));
+	for(char* i=inBuffer; i < (inBuffer + sizeof(inBuffer)); i++){
+		*i = '\0';
+	}
+}
--- GsmWrap.h
+++ GsmWrap.h
@@ -1,21 +1,57 @@
+#ifndef GSMWRAP_H
+#define GSMWRAP_H
+
+#include <Arduino.h>
 #include <SoftwareSerial.h>
+
+#define BUFFSIZE_IN 256
+
+#define SERIAL_READ_TIMEOUT 5000
 
 #define STD_RX_PIN 4
 #define STD_TX_PIN 5
 #define STD_BAUD 9600
 
+/*struct GsmWrap_datetime{
+	int year;
+	int month;
+	int day;
+	int hour;
+	int minute;
+	int second;
+};
+
+enum GsmWrap_statusSMS {UNREAD, READ, UNSENT, SENT};
+
+struct GsmWrap_sms{
+	int index;
+	char origin[];
+	GsmWrap_statusSMS status;
+	GsmWrap_datetime recieveTime;
+};*/
+
 class GsmWrap{
 	public:
-		GsmWrap();
-		GsmWrap(int rxPin, int txPin, int baud);
-		~GsmWrap();
+		GsmWrap(SoftwareSerial* softser);
+		//~GsmWrap();
 		
+		void begin(int baud);
+		bool serialAvailable();
+		int serialRead();
+		void serialWrite(const char* message);
+		char* getInBuffer();
+		void printBuffer();
+		
+		
+		//int countSMS();
+		//GsmWrap_sms readSMS(int index);
+		//bool sendSMS(char[] number, char[] text);
 		
 	private:
-		beginSerial();
-	
-		int rx;
-		int tx;
-		int baudrate;
-		SoftwareSerial ss;
-}
+		void clearInBuffer();
+		
+		SoftwareSerial* ss;
+		char inBuffer[BUFFSIZE_IN];
+};
+
+#endif //GSMWRAP_H
